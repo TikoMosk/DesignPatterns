210ab8391c228ea8be3eb42e7e9ad395f593a6bd
Tigran Ghukasian <urakhblog3@gmail.com>
2019-07-03 10:24:02 +0400

    first commit

---
+++ Assets/Scripts/AbstractFactoryMethod.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractFactoryMethod : MonoBehaviour
+{
+    private static AbstractFactoryMethod _instance;
+    public static AbstractFactoryMethod Instance
+    {
+        get { return _instance; }
+    }
+    
+    [System.Serializable]
+    public struct ColoredTree
+    {
+        public Tree.TreeColor color;
+        public List<GameObject> treePrefabs;
+    }
+    public List<ColoredTree> coloredTreeList;
+
+    public GameObject GetAppropriatePrefab(Tree tree)
+    {
+        foreach (ColoredTree s in coloredTreeList)
+        {
+            if(s.color == tree.Color)
+            {
+                return s.treePrefabs[tree.TreeType];
+            }
+        }
+        Debug.Log("No match");
+        return null;
+    }
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    void Start()
+    {
+        TreeFactory[] coloredTreeFactories = new TreeFactory[3];
+        coloredTreeFactories[0] = new GreenTreeFactory();
+        coloredTreeFactories[1] = new BlueTreeFactory();
+        coloredTreeFactories[2] = new OrangeTreeFactory();
+
+        foreach(TreeFactory colorFactory in coloredTreeFactories)
+        {
+            foreach(Tree t in colorFactory.TreeList)
+            {
+                Instantiate(t.TreePrefab, t.Position, Quaternion.identity);
+            }
+        }
+    }
+}
+abstract class TreeFactory
+{
+    private List<Tree> treeList = new List<Tree>();
+    public List<Tree> TreeList
+    {
+        get { return treeList; }
+    }
+    public TreeFactory()
+    {
+        this.CreatePineTree();
+        this.CreateTallpineTree();
+        this.CreateTallTree();
+    }
+    public abstract void CreatePineTree();
+    public abstract void CreateTallpineTree();
+    public abstract void CreateTallTree();
+}
+class GreenTreeFactory : TreeFactory
+{
+    public override void CreatePineTree()
+    {
+        TreeList.Add(new PineTree(Tree.TreeColor.Green, new Vector3(0, 0, 0)));
+    }
+    public override void CreateTallpineTree()
+    {
+        TreeList.Add(new TallpineTree(Tree.TreeColor.Green, new Vector3(4, 0, 0)));
+    }
+    public override void CreateTallTree()
+    {
+        TreeList.Add(new TallTree(Tree.TreeColor.Green, new Vector3(8, 0, 0)));
+    }
+}
+class BlueTreeFactory : TreeFactory
+{
+    public override void CreatePineTree()
+    {
+        TreeList.Add(new PineTree(Tree.TreeColor.Blue, new Vector3(0, 0, 4)));
+    }
+    public override void CreateTallpineTree()
+    {
+        TreeList.Add(new TallpineTree(Tree.TreeColor.Blue, new Vector3(4, 0, 4)));
+    }
+    public override void CreateTallTree()
+    {
+        TreeList.Add(new TallTree(Tree.TreeColor.Blue, new Vector3(8, 0, 4)));
+    }
+}
+
+class OrangeTreeFactory : TreeFactory
+{
+    public override void CreatePineTree()
+    {
+        TreeList.Add(new PineTree(Tree.TreeColor.Orange,new Vector3(0,0,8)));
+    }
+    public override void CreateTallpineTree()
+    {
+        TreeList.Add(new TallpineTree(Tree.TreeColor.Orange, new Vector3(4, 0, 8)));
+    }
+    public override void CreateTallTree()
+    {
+        TreeList.Add(new TallTree(Tree.TreeColor.Orange, new Vector3(8, 0,8)));
+    }
+}
+public abstract class Tree
+{
+    private Vector3 position;
+    public Vector3 Position
+    {
+        get { return position; }
+    }
+    public enum TreeColor { Green, Blue, Orange };
+    private TreeColor color;
+    public TreeColor Color
+    {
+        get { return color; }
+    }
+    internal int treeType;
+    public int TreeType
+    {
+        get { return treeType; }
+    }
+    private GameObject treePrefab;
+    public GameObject TreePrefab
+    {
+        get { return treePrefab; }
+    }
+    public Tree(TreeColor _treeColor, Vector3 _position, int _treeType)
+    {
+        color = _treeColor;
+        position = _position;
+        treeType = _treeType;
+        MatchColorWithPrefab();
+        
+    }
+    protected void MatchColorWithPrefab()
+    {
+        treePrefab = AbstractFactoryMethod.Instance.GetAppropriatePrefab(this);
+    }
+}
+class PineTree : Tree
+{
+    public PineTree(TreeColor _color, Vector3 _position) : base(_color, _position, 0)
+    {
+
+    }
+}
+class TallpineTree : Tree
+{
+    public TallpineTree(TreeColor _color, Vector3 _position) : base(_color,_position, 1)
+    {
+
+    }
+}
+class TallTree : Tree
+{
+    public TallTree(TreeColor _color, Vector3 _position) : base(_color, _position, 2)
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/FactoryMethod.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryMethod : MonoBehaviour
+{
+    Environment[] environments = new Environment[2];
+    public GameObject treePrefab;
+    public GameObject rockPrefab;
+
+    void Start()
+    {
+        environments[0] = new ForestEnvironment();
+        environments[1] = new RockyEnvironment();
+
+        foreach(Environment env in environments)
+        {
+            GameObject environmentGO = new GameObject(env.ToString());
+            foreach(EnvObject envObj in env.EnvironmentObjects)
+            {
+                GameObject envGameObject = treePrefab;
+                if(envObj is EnvTree)
+                {
+                    envGameObject = Instantiate(treePrefab, envObj.Position, Quaternion.identity);
+                }
+                else if(envObj is EnvRock)
+                {
+                    envGameObject = Instantiate(rockPrefab, envObj.Position, Quaternion.identity);
+                }
+                envGameObject.transform.parent = environmentGO.transform;
+                Debug.Log("Spawn " + envObj.ToString() + " at the coordinates" + envObj.Position + " In the " + env.ToString() + " environment");
+            }
+        }
+    }
+    
+}
+abstract class Environment
+{
+    private List<EnvObject> _environmentObjects = new List<EnvObject>();
+    private Vector3 position;
+    public Vector3 Position
+    {
+        get { return position; }
+    }
+    public Environment()
+    {
+        this.CreateEnvironment();
+    }
+    public List<EnvObject> EnvironmentObjects
+    {
+        get { return _environmentObjects; }
+    }
+    public abstract void CreateEnvironment();
+}
+class ForestEnvironment : Environment
+{
+    public override void CreateEnvironment()
+    {
+        EnvironmentObjects.Add(new EnvTree(new Vector3(0,0,0)));
+    }
+}
+class RockyEnvironment : Environment
+{
+    public override void CreateEnvironment()
+    {
+        EnvironmentObjects.Add(new EnvRock(new Vector3(0, 0, 10)));
+        EnvironmentObjects.Add(new EnvRock(new Vector3(0, 0, 8)));
+        EnvironmentObjects.Add(new EnvRock(new Vector3(2, 0, 7)));
+        EnvironmentObjects.Add(new EnvRock(new Vector3(0, 0, 5)));
+    }
+}
+abstract class EnvObject
+{
+    protected Vector3 position = Vector3.zero;
+    public Vector3 Position
+    {
+        get { return position; }
+    }
+}
+class EnvTree : EnvObject
+{
+    public EnvTree(Vector3 _position)
+    {
+        
+        position = _position;
+    }
+}
+class EnvRock : EnvObject
+{
+    public EnvRock(Vector3 _position)
+    {
+        
+        position = _position;
+    }
+}
